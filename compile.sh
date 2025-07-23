9e8e610f1b53d467c5618a39938ca8bea37cb4e8
Francis Grizzly Smit <grizzly@smit.id.au>
2025-07-23 14:44:51 +1000

    commited with save.raku 2025-07-23T14:44:51.593209+10:00

--- compile.sh
+++ compile.sh
@@ -7,6 +7,16 @@ SUFFIX="po"
 
 xgettext --from-code='utf-8' -k_ -kN_ -o $DIR/hplip-menu2.pot prefs.js extension.js gzzDialog.js CompactMenu.js
 
+if ! [ -x "$DIR"/en.$SUFFIX ]
+then
+    msginit --input="$DIR"/hplip-menu2.pot --output-file="$DIR"/en.po --no-translator
+else
+    for file in "$DIR"/*.$SUFFIX 
+    do
+        msgmerge --previous --update "$DIR"/$file "$DIR"/hplip-menu2.pot
+    done
+fi
+
 for file in "$DIR"/*.$SUFFIX
 do 
 	lingua=${file%.*}
